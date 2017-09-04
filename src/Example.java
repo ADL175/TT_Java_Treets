8b1e6cb4b1b13bc1aec37fb027b87c1414c0dfb4
ADL175 <armydavidlim@gmail.com>
2017-09-03 20:35:32 -0700

    Initial commit

--- src/Example.java
+++ src/Example.java
@@ -1,37 +1,25 @@
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
 import com.teamtreehouse.Treet;
 import com.teamtreehouse.Treets;
 
 
-
 public class Example {
+
     public static void main(String[] args) {
-        Treet treet = new Treet(
-                "David Lim",
-                "Join us for the first annual Veterans in Tech Summit where Seattle’s top technology, corporate, education, and civic leaders will showcase the innovation and opportunities available with the region’s military presence.",
-                new Date(1504229850L)
-        );
-        Treet secondTreet = new Treet(
-                "journeytocode",
-                "@Treehouse makes learning Java so fun.",
-                new Date()
-        );
-        System.out.printf("This is a new Treet: %s %n", treet);
-        System.out.println("The words are: ");
-        for (String word: treet.getWords()) {
-            System.out.println(word);
+        Treet[] treets = Treets.load();
+        System.out.printf("There are %d treets. %n",
+                treets.length);
+        Set<String> allHashTags = new HashSet<>();
+        Set<String> allMentions = new TreeSet<>();
+        for (Treet treet : treets) {
+            allHashTags.addAll(treet.getHashTags());
+            allMentions.addAll(treet.getMentions());
         }
-        Treet[] treets = {treet, secondTreet };
-        Arrays.sort(treets);
-        for (Treet exampleTreet : treets) {
-            System.out.println(exampleTreet);
-        }
-        Treets.save(treets);
-        Treet[] reloadedTreets = Treets.load();
-        for (Treet reloaded : reloadedTreets) {
-            System.out.println(reloaded);
-        }
+        System.out.printf("Hashtags: %s %n",
+                allHashTags);
+        System.out.printf("Mentions: %s %n",
+                allMentions);
     }
+
 }
